c96e34e78aa8610769f380c05b88216154bdd0fb
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-03-15 22:29:01 +0530

    Update gcd.cpp

--- PRACTICE CODES/gcd.cpp
+++ PRACTICE CODES/gcd.cpp
@@ -1,1 +1,11 @@
 //here you can write your code.
+#include<iostream>
+#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{   int a,b;
+	cin>>a>>b;
+	cout<<__gcd(a,b);
+    return 0;
+}
