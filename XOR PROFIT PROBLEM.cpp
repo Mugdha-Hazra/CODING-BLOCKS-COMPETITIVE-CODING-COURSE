b6bae9d1f1de609a9cdedca634a3118dbe469236
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-04-10 20:42:26 +0530

    Update XOR PROFIT PROBLEM.cpp

--- XOR PROFIT PROBLEM.cpp
+++ XOR PROFIT PROBLEM.cpp
@@ -1,17 +1,17 @@
 //Maximum XOR value of a pair from a range
+/* Here we will xor both the given numbers and then count the number of digits.
+Then maximum value would be the all ones int the binary representation so will calculate the power of pow(2,c)-1;
+*/
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 int main () 
-{
-	int x,y,p,c=0;
+{       int x,y,p,c=0;
 	cin>>x>>y;
 	p=x^y;
 	while(p)
-	{
-		c++;
-		p=p>>1;
-	}
+	{c++;
+	 p=p>>1;}
 	cout<<pow(2,c)-1<<"\n";
 	return 0;
 }
