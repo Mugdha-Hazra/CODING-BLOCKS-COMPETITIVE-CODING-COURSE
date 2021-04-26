e4573d1922be66434372bdf5f9f058703088ab2c
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-04-26 16:57:30 +0530

    Update bitwise operator overloading.cpp

--- PRACTICE CODES/bitwise operator overloading.cpp
+++ PRACTICE CODES/bitwise operator overloading.cpp
@@ -1,3 +1,18 @@
+//C++ Code for Bitwise operator overloading
+
+/*
+      Approach
+      =========
+     1. We can used User-defined classes and enumerations that implement the requirements
+        of BitmaskType are required to overload the bitwise arithmetic operators.
+      
+     2. operator&, operator|, operator^,and may optionally overload the shift operators operator<< operator>>.. 
+        Overloaded bitwise operators should be non-members to allow for operands of different types
+        , e.g. a fundamental type and a class type, or two unrelated class types.
+      
+     3. A non-const overload of the subscript operator should allow an object to be modified,
+        i.e. should return a reference to member data.
+*/
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -43,9 +58,9 @@ class bitwise
 };
 int main() 
 {
-               int a,b;
-               cout<<"Enter 2 numbers:";
-               cin>>a>>b;
+        int a,b;
+        cout<<"Enter 2 numbers:";
+        cin>>a>>b;
         bitwise b1(a);
         bitwise b2(b);
         bitwise b3=b1&b2;
@@ -66,3 +81,53 @@ int main()
         b6.show();
                return 0;
 }
+
+/*
+      Time Complexity: O(1) 
+      
+      Sample Input/Output
+      =======================
+      
+      Sample Input : 1 
+      10 20
+      
+      sample Output : 1
+      
+      Enter 2 numbers:
+      NUMBER 1  = 10
+      NUMBER 2  = 20
+      NUMBER1 & NUMBER2  = 0
+      NUMBER1 | NUMBER2  = 30
+      NUMBER1 ^ NUMBER2  = 30
+      NUMBER1 >> NUMBER2  = 0
+      
+      Sample Input : 2
+      10 2
+      
+      sample Output : 2
+      
+      Enter 2 numbers:
+      NUMBER 1  = 10
+      NUMBER 2  = 2
+      NUMBER1 & NUMBER2  = 2
+      NUMBER1 | NUMBER2  = 10
+      NUMBER1 ^ NUMBER2  = 8
+      NUMBER1 >> NUMBER2  = 2
+      
+      Sample Input : 3
+      5 7
+      
+      sample Output : 2
+      
+      Enter 2 numbers:
+      NUMBER 1  = 5
+      NUMBER 2  = 7
+      NUMBER1 & NUMBER2  = 5
+      NUMBER1 | NUMBER2  = 7
+      NUMBER1 ^ NUMBER2  = 2
+      NUMBER1 >> NUMBER2  = 0
+      
+      
+
+      
+*/
