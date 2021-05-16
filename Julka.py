77d5483ad57b0dc76e3283783fb06a64ba448bf6
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-05-16 12:18:17 +0530

    Create Julka.py

---
+++ Julka.py
@@ -0,0 +1,11 @@
+# cook your dish here
+count=0;
+ 
+while(count<10):
+    count+=1
+    total=int(input());
+    diff=int(input());
+    natallia=(total-diff)//2;
+    klaudia=natallia+diff;
+    print(klaudia);
+    print(natallia); 
