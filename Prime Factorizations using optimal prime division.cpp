9d5a35641508f02c29c701632d209f28f7bee607
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-05-20 13:16:17 +0530

    Update Prime Factorizations using optimal prime division.cpp

--- Prime Factorizations using optimal prime division.cpp
+++ Prime Factorizations using optimal prime division.cpp
@@ -8,7 +8,7 @@ void printFactor(int n)
     for(int i=2;i*i<=n;i++)
     {
         if(n%i==0)
-        {
+        {    //keep on dividing it by i till it is divisible
             cnt=0;
             while(n%i==0)
             {
@@ -19,9 +19,10 @@ void printFactor(int n)
         }
     }
     if(n!=1)
-    {
+    {   //whatever is left is a prime number in itself.
         factors.push_back(make_pair(n,1));
     }
+    //print the factors
     for(auto p:factors)
     {
         cout<<p.first<<"^"<<p.second<<", ";
