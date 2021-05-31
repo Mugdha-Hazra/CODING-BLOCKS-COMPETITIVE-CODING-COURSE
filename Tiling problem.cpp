2685bb7386602948bf40a0e4084e5e158c19cd30
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2021-05-31 12:10:23 +0530

    Update Tiling problem.cpp

--- Tiling problem.cpp
+++ Tiling problem.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 int replaceTile(long long int m,long long int n,long long int s)
@@ -13,3 +14,42 @@ int main()
     cin>>n>>m;
     cout <<replaceTile(m,n,0)%1000000007<<"\n";
 	}return 0;}
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstring>
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+
+
+ll compute(ll n, ll m){
+    // Base Case
+    int score[n+1];
+    score[0] = 0;
+    score[1] = 1;
+    for(int j=2; j<=n;j++){
+        // Checks:
+        if (m>j) {
+            score[j] = 1;
+        }
+        else if (m==j){
+        score[j] = 2;
+        }
+        else {
+        score[j] = (score[j-m]+score[j-1])%MOD;
+        }
+    }
+    return score[n];
+}
+
+
+int main(){
+    ll t,n,m;
+    cin >> t;
+    for (int i=0;i<t;i++){
+        cin >> n >> m;
+        ll ans = compute(n,m);
+        cout << ans%MOD << endl;
+    }
+}
